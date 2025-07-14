ce804638e24207c2b1388a5c51c3925599b07339
scottmckenzie <sccmckenzie@gmail.com>
2025-07-13 20:59:39 -0500

    provision grid: add index

--- provision-grid.py
+++ provision-grid.py
@@ -9,6 +9,7 @@ lf0 = pl.LazyFrame({"x": v})
 out = (lf0
        .join(lf0.rename({"x": "y"}), how="cross")
        .filter(pl.col("x").pow(2) + pl.col("y").pow(2) <= scale**2)
+       .with_row_index(name="grid_id", offset=1)
        .collect())
 
-out.write_csv("dbt_spatial_config/grid.csv")
+out.write_csv("dbt_spatial_config/seeds/grid.csv")
