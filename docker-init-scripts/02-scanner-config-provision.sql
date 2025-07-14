4c9b0921ba1740ce9f18473ed061d1be53b98e40
scottmckenzie <sccmckenzie@gmail.com>
2025-07-13 20:59:30 -0500

    db permissions amend

--- docker-init-scripts/02-scanner-config-provision.sql
+++ docker-init-scripts/02-scanner-config-provision.sql
@@ -1,3 +1,4 @@
 create schema config;
+grant usage on schema config to scanner_rw;
 grant select on all tables in schema config to scanner_rw;
 alter default privileges in schema config grant select on tables to scanner_rw;
