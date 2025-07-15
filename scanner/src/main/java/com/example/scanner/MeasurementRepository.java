b8e359391c9b06a5c3d89ed3572fea6cb1fdddf1
scottmckenzie <sccmckenzie@gmail.com>
2025-07-14 22:20:48 -0500

    remove unnecessary comments

--- scanner/src/main/java/com/example/scanner/MeasurementRepository.java
+++ scanner/src/main/java/com/example/scanner/MeasurementRepository.java
@@ -1,9 +1,7 @@
 package com.example.scanner;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
     // You can add custom query methods here if needed
 }
