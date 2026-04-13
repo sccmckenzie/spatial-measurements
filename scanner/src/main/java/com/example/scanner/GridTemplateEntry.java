4abe2477cbe0381a65794b62d9cc95ecd7749358
scottmckenzie <sccmckenzie@gmail.com>
2026-04-12 20:44:03 -0500

    GridTemplateEntry.java: add lombok setter

--- scanner/src/main/java/com/example/scanner/GridTemplateEntry.java
+++ scanner/src/main/java/com/example/scanner/GridTemplateEntry.java
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Immutable;
 
 @Entity
 @Getter
+@Setter
 @Table(schema = "config", name = "grid")
 @Immutable
 public class GridTemplateEntry {
