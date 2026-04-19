0eaecb1753366451916deb32fc74e8f75e4ca56c
scottmckenzie <sccmckenzie@gmail.com>
2026-04-19 15:33:56 -0500

    remove test schema from main postgres instance

--- docker-init-scripts/01-scanner-writer-provision.sql
+++ docker-init-scripts/01-scanner-writer-provision.sql
@@ -1,9 +1,6 @@
 create user scanner_rw with password 'brownies';
 create schema raw_measurements;
-create schema raw_measurements_test;
 create sequence raw_measurements.scan_id_sequence START WITH 1 INCREMENT BY 1;
 grant usage on sequence raw_measurements.scan_id_sequence to scanner_rw;
 grant all privileges on schema raw_measurements to scanner_rw;
-grant all privileges on schema raw_measurements_test to scanner_rw;
 alter default privileges in schema raw_measurements grant all privileges on tables to scanner_rw;
-alter default privileges in schema raw_measurements_test grant all privileges on tables to scanner_rw;
