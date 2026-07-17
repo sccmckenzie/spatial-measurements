84e6346011978432f91eef7998d5a32ffe5a8ec5
scottmckenzie <sccmckenzie@gmail.com>
2026-07-16 20:52:25 -0500

    batch-load-duckdb.py: remove

--- batch-load-duckdb.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-
-# Connect duckdb to postgres
-con = 'host=localhost port=5432 dbname=postgres connect_timeout=10 user=duckdb_ro password=applesauce'
-duckdb.sql(f"ATTACH '{con}' AS postgres_db (TYPE postgres)")
-
-# batch load measurement records into parquet file
-copy_statement = "copy (select * from postgres_db.raw.measurement limit 5) TO 'lineitem.parquet' (FORMAT parquet, APPEND true)"
-duckdb.sql(copy_statement)
